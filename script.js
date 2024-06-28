d90d6099f5d91fc305a66bc9219fd61de3b5e1e6
Moa Davou <moa.davou99@gmail.com>
2024-06-28 09:35:33 +0200

    Improved accessibility & formatted code

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-const shareButton = document.getElementById("share-button");
-const shareButtonPopup = document.getElementById("share-button-popup");
+const shareButton = document.getElementById('share-button');
+const shareButtonPopup = document.getElementById('share-button-popup');
 
 // Hides shareButtonPopup
 function closeShareButtonPopup() {
-    shareButton.setAttribute("aria-expanded", "false");
+  shareButton.setAttribute('aria-expanded', 'false');
 }
 
 // Shows shareButtonPopup and focuses the first link
 function openShareButtonPopup() {
-    shareButton.setAttribute("aria-expanded", "true");
+  shareButton.setAttribute('aria-expanded', 'true');
 
-    const shareButtonPopupFirstLink = shareButtonPopup.querySelector("a");
-    if (shareButtonPopupFirstLink) {
-        shareButtonPopupFirstLink.focus();
-    }
+  const shareButtonPopupFirstLink = shareButtonPopup.querySelector('a');
+  if (shareButtonPopupFirstLink) {
+    shareButtonPopupFirstLink.focus();
+  }
 }
 
 // Toggles shareButtonPopup on click
-shareButton.addEventListener("click", () => {
-    if (shareButton.getAttribute("aria-expanded") === "false") {
-        openShareButtonPopup();
-    } else {
-        closeShareButtonPopup();
-    }
+shareButton.addEventListener('click', () => {
+  if (shareButton.getAttribute('aria-expanded') === 'false') {
+    openShareButtonPopup();
+  } else {
+    closeShareButtonPopup();
+  }
 });
 
 // Close shareButtonPopup with escape key
-document.addEventListener("keydown", (event) => {
-    if (event.key === "Escape") {
-        closeShareButtonPopup();
-    }
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeShareButtonPopup();
+  }
 });
 
 // Close shareButtonPopup when user clicks outside of shareButtonPopup
-document.addEventListener("click", (event) => {
-    if (
-        !shareButton.contains(event.target) &&
-        !shareButtonPopup.contains(event.target)
-    ) {
-        closeShareButtonPopup();
-    }
+document.addEventListener('click', (event) => {
+  if (
+    !shareButton.contains(event.target) &&
+    !shareButtonPopup.contains(event.target)
+  ) {
+    closeShareButtonPopup();
+  }
 });
