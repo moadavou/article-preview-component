b5361198a6385622b8d45f30a4fd3814cbadff7f
Moa Davou <moa.davou99@gmail.com>
2024-07-03 10:53:06 +0200

    Add inert attribute to card author

--- script.js
+++ script.js
@@ -1,43 +1,51 @@
-const shareButton = document.getElementById('share-button');
-const shareButtonPopup = document.getElementById('share-button-popup');
+const shareBtn = document.getElementById('share-button');
+const shareBtnPopup = document.getElementById('share-button-popup');
+const cardAuthor = document.getElementById('author');
 
 // Hides shareButtonPopup
-function closeShareButtonPopup() {
-  shareButton.setAttribute('aria-expanded', 'false');
+function closeShareBtnPopup() {
+  shareBtn.setAttribute('aria-expanded', 'false');
+
+  if (
+    window.matchMedia('(max-width: 60rem)').matches &&
+    cardAuthor.hasAttribute('inert')
+  ) {
+    cardAuthor.removeAttribute('inert', '');
+  }
 }
 
-// Shows shareButtonPopup and focuses the first link
-function openShareButtonPopup() {
-  shareButton.setAttribute('aria-expanded', 'true');
+// Shows shareButtonPopup
+function openShareBtnPopup() {
+  shareBtn.setAttribute('aria-expanded', 'true');
 
-  const shareButtonPopupFirstLink = shareButtonPopup.querySelector('a');
-  if (shareButtonPopupFirstLink) {
-    shareButtonPopupFirstLink.focus();
+  if (window.matchMedia('(max-width: 60rem)').matches) {
+    cardAuthor.setAttribute('inert', '');
+    console.log('Inert attribute set:', cardAuthor.getAttribute('inert'));
   }
 }
 
 // Toggles shareButtonPopup on click
-shareButton.addEventListener('click', () => {
-  if (shareButton.getAttribute('aria-expanded') === 'false') {
-    openShareButtonPopup();
+shareBtn.addEventListener('click', () => {
+  if (shareBtn.getAttribute('aria-expanded') === 'false') {
+    openShareBtnPopup();
   } else {
-    closeShareButtonPopup();
+    closeShareBtnPopup();
   }
 });
 
 // Close shareButtonPopup with escape key
 document.addEventListener('keydown', (event) => {
   if (event.key === 'Escape') {
-    closeShareButtonPopup();
+    closeShareBtnPopup();
   }
 });
 
 // Close shareButtonPopup when user clicks outside of shareButtonPopup
 document.addEventListener('click', (event) => {
   if (
-    !shareButton.contains(event.target) &&
-    !shareButtonPopup.contains(event.target)
+    !shareBtn.contains(event.target) &&
+    !shareBtnPopup.contains(event.target)
   ) {
-    closeShareButtonPopup();
+    closeShareBtnPopup();
   }
 });
